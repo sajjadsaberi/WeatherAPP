814953bd979c25ed7b219926770b3019d36445a6
Sajjad Saberi <sajjad.saberi1996@gmail.com>
2023-10-01 22:05:52 +0330

    complete api weather in this app

--- script.js
+++ script.js
@@ -3,18 +3,23 @@ const API_KEY = "3b25d17acf02c0482f0ad00e907b96ec" ;
 const searchInput = document.querySelector("input") ;
 const searchButton = document.querySelector("button") ;
 
-const getCurrentWeatherByName = (city) => {
-    const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}`;
-    console.log(url)
+const getCurrentWeatherByName = async (city) => {
+    const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    const response = await fetch(url);
+    const json = await response.json() ;
+    return json ;
 }
 
-const searchHandler = () => {
+const renderCurrentWeather = data => {console.log()}
+
+const searchHandler = async () => {
     const cityName = searchInput.value 
 
     if(!cityName) {
         alert("Please Enter City Name") ;
     }
-    getCurrentWeatherByName(cityName);
+    const currentData = await getCurrentWeatherByName(cityName);
+    console.log(currentData)
 };
 
 searchButton.addEventListener("click", searchHandler) ;
