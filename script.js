182af15c27b6e08d86f0195e90bf80f8ca370ae8
Sajjad Saberi <sajjad.saberi1996@gmail.com>
2023-10-01 21:18:36 +0330

    add the firs api for this project

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const api = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}" ;
-const apiKey = "248b7fe5e5403295a1156734f257171f" ;
+const BASE_URL = "https://api.openweathermap.org/data/2.5"
+const API_KEY = "3b25d17acf02c0482f0ad00e907b96ec" ;
+const searchInput = document.querySelector("input") ;
+const searchButton = document.querySelector("button") ;
+
+const getCurrentWeatherByName = (city) => {
+    const url = `${BASE_URL}/weather?q=${city}&appid=${API_KEY}`;
+    console.log(url)
+}
+
+const searchHandler = () => {
+    const cityName = searchInput.value 
+
+    if(!cityName) {
+        alert("Please Enter City Name") ;
+    }
+    getCurrentWeatherByName(cityName);
+};
+
+searchButton.addEventListener("click", searchHandler) ;
